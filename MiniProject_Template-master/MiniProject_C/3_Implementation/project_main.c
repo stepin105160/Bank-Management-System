51fe9dfa48d5861d13031c24044ffaacc08c49e6
stepin105160 <69168813+stepin105160@users.noreply.github.com>
2020-10-08 18:47:39 +0530

    Add files via upload

---
+++ MiniProject_Template-master/MiniProject_C/3_Implementation/project_main.c
@@ -0,0 +1,94 @@
+#include <Bank_Management_System.h>
+
+int i,j;
+int main_exit;
+void menu();
+
+void menu(void)
+{   int choice;
+    system("cls");
+    system("color 7");
+    printf("\n\n\t\t\t       BANKING MANAGEMENT SYSTEM");
+    printf("\n\n\n\t\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2 WELCOME TO THE MAIN MENU \xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+    printf("\n\n\t\t[1] Create a new account\n\t\t[2] Update information of existing account\n\t\t[3] Transactions\n\t\t[4] Check the details of existing account\n\t\t[5] Remove existing account\n\t\t[6] View customer's list\n\t\t[7] ATM Feature\n\t\t[8] Exit\n\n\n\t\t Enter your choice:");
+    scanf("%d",&choice);
+
+    system("cls");
+    switch(choice)
+    {
+        case 1:new_acc();
+        break;
+        case 2:edit();
+        break;
+        case 3:transact();
+        break;
+        case 4:see();
+        break;
+        case 5:erase();
+        break;
+        case 6:view_list();
+        break;
+        case 7:atm();
+        break;
+        case 8:close();
+        break;
+    }
+
+
+
+}
+int main()
+{
+    char pass[10],password[10]="pass";
+    int i=0;
+    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login ");
+    printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
+    scanf("%s",pass);
+    /*do
+    {
+    //if (pass[i]!=13&&pass[i]!=8)
+        {
+            printf("*");
+            pass[i]=getch();
+            i++;
+        }
+    }while (pass[i]!=13);
+    pass[10]='\0';*/
+    if (strcmp(pass,password)==0)
+        {printf("\n\nPassword Match!\nLOADING");
+        for(i=0;i<=6;i++)
+        {
+            fordelay(100000000);
+            printf(". ");
+        }
+        printf("\n");
+                system("pause");
+				system("cls");
+
+            menu();
+        }
+    else
+        {   printf("\n\nWrong password!!\a\a\a");
+            login_try:
+            printf("\nEnter 1 to try again and 0 to exit:");
+            scanf("%d",&main_exit);
+            if (main_exit==1)
+                    {
+
+                        system("cls");
+                        main();
+                    }
+
+            else if (main_exit==0)
+                    {
+                    system("cls");
+                    close();}
+            else
+                    {printf("\nInvalid!");
+                    fordelay(1000000000);
+                    system("cls");
+                    goto login_try;}
+
+        }
+        return 0;
+}
